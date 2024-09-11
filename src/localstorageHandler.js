a8832ce4a85bc0c8a48b20d28d569b1698df95cf
pickingname <trintrinir@icloud.com>
2024-09-11 21:27:57 +0700

    refactor: remove unused variables and improve error handling in localstorageHandler.js

--- src/localstorageHandler.js
+++ src/localstorageHandler.js
@@ -1,42 +1,34 @@
-function isFirstRun() {
-  const firstRunKey = "isFirstRun";
-  const apiTypeKey = "apiType";
-  const themeSettingKey = "themeSetting";
-  const hideLegendKey = "hideLegend";
-  const canMapBeMovedKey = "canMapBeMoved";
-  const geoJsonMapKey = "geoJsonMap";
-  const timeConversionKey = "timeConversion";
+const firstRunKey = "isFirstRun";
+const apiTypeKey = "apiType";
+const themeSettingKey = "themeSetting";
+const hideLegendKey = "hideLegend";
+const canMapBeMovedKey = "canMapBeMoved";
+const geoJsonMapKey = "geoJsonMap";
+const timeConversionKey = "timeConversion";
 
-  // default values
-  const defaultSettings = {
-    [firstRunKey]: "false",
-    [apiTypeKey]: "main",
-    [themeSettingKey]: "system",
-    [hideLegendKey]: "false",
-    [canMapBeMovedKey]: "false",
-    [geoJsonMapKey]: "false",
-    [timeConversionKey]: "true",
-  };
+// default values
+const defaultSettings = {
+  [firstRunKey]: "false",
+  [apiTypeKey]: "main",
+  [themeSettingKey]: "system",
+  [hideLegendKey]: "false",
+  [canMapBeMovedKey]: "false",
+  [geoJsonMapKey]: "false",
+  [timeConversionKey]: "true",
+};
 
-  try {
-    // localStorage feature check
-    if (typeof localStorage === "undefined") {
-      console.warning("localStorage is not available in this environment.");
-      return false;
-    }
-
-    let isAnyKeyMissing = false;
-
-    for (const key in defaultSettings) {
-      if (!localStorage.getItem(key)) {
-        localStorage.setItem(key, defaultSettings[key]);
-        isAnyKeyMissing = true;
-      }
-    }
-
-    return isAnyKeyMissing;
-  } catch (error) {
-    console.error("An error occurred while accessing localStorage:", error);
-    return false;
+try {
+  // localStorage feature check
+  if (typeof localStorage === "undefined") {
+    console.warning("localStorage is not available in this environment.");
   }
+
+  for (const key in defaultSettings) {
+    if (!localStorage.getItem(key)) {
+      localStorage.setItem(key, defaultSettings[key]);
+    }
+  }
+
+} catch (error) {
+  console.error("An error occurred while accessing localStorage:", error);
 }
