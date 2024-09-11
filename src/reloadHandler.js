abd3102de2fe1555bf425c5312e34ee457c26cbc
pickingname <trintrinir@icloud.com>
2024-09-11 20:41:39 +0700

    refractor: fix useless template literal

--- src/reloadHandler.js
+++ src/reloadHandler.js
@@ -20,7 +20,7 @@ export function dimScreenAndReload() {
 
     const spinner = document.createElement("div");
     spinner.classList.add("mx-auto");
-    spinner.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="stroke-white animate-spin loadingSpinner"><path d="M21 12a9 9 0 1 1-6.219-8.56"/></svg>`;
+    spinner.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="stroke-white animate-spin loadingSpinner"><path d="M21 12a9 9 0 1 1-6.219-8.56"/></svg>';
     contentWrapper.appendChild(spinner);
 
     const bottomText = document.createElement("p");
