4194e9159541fbb7a55d284b4e79c3450487819a
pickingname <trintrinir@icloud.com>
2024-09-14 18:00:45 +0700

    feat: first iteration of the epicenter pref int calc

--- src/maphandler.js
+++ src/maphandler.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import Papa from "papaparse";
+import * as turf from '@turf/turf';
 import { dimScreenAndReload } from "./reloadHandler";
 import {
   initCircleRendering,
@@ -41,13 +42,13 @@ const leaflet = L;
 
 const newData = new Audio("https://pickingname.github.io/datastores/yes.mp3");
 const intensityReport = new Audio(
-  "https://pickingname.github.io/datastores/update.mp3",
+  "https://pickingname.github.io/datastores/update.mp3"
 );
 const distantArea = new Audio(
-  "https://pickingname.github.io/datastores/alert.mp3",
+  "https://pickingname.github.io/datastores/alert.mp3"
 );
 const tsunamiWarning = new Audio(
-  "https://pickingname.github.io/datastores/eq/E4.mp3",
+  "https://pickingname.github.io/datastores/eq/E4.mp3"
 );
 
 let previousEarthquakeData = null;
@@ -57,13 +58,50 @@ let stationMarkersGroup = null;
 let tsunamiGeojsonLayer = null;
 let usegeojson = "false"; // needs init on exec anyway and needs to be false on default
 
+const intensityColors = { // temp colors
+  '1': '#ffffff',
+  '2': '#b2ffff',
+  '3': '#00ffff',
+  '4': '#ffd700',
+  '5-': '#ffa500',
+  '5+': '#ff8c00',
+  '6-': '#ff4500',
+  '6+': '#ff0000',
+  '7': '#8b0000'
+};
+
+function calculateJMAIntensity(epicenterLat, epicenterLon, depth, magnitude, pointLat, pointLon) {
+  const distance = turf.distance(
+    turf.point([epicenterLon, epicenterLat]),
+    turf.point([pointLon, pointLat]),
+    { units: 'kilometers' }
+  );
+
+  const hypocentralDistance = Math.sqrt(Math.pow(distance, 2) + Math.pow(depth, 2));
+  const pga = Math.exp(0.58 * magnitude - 1.83 * Math.log10(hypocentralDistance) - 0.0036 * hypocentralDistance);
+
+
+  let intensity;
+  if (pga < 0.008) intensity = '1';
+  else if (pga < 0.025) intensity = '2';
+  else if (pga < 0.08) intensity = '3';
+  else if (pga < 0.25) intensity = '4';
+  else if (pga < 0.8) intensity = '5-';
+  else if (pga < 1.4) intensity = '5+';
+  else if (pga < 2.5) intensity = '6-';
+  else if (pga < 4.0) intensity = '6+';
+  else intensity = '7';
+
+  return intensity;
+}
+
 if (localStorage.getItem("geoJsonMap") === "true") {
   usegeojson = true;
 } else if (localStorage.getItem("geoJsonMap") === "false") {
   usegeojson = false;
 } else {
   console.log(
-    `geoJsonMap is ${localStorage.getItem("geoJsonMap")}, defaulting to false`,
+    `geoJsonMap is ${localStorage.getItem("geoJsonMap")}, defaulting to false`
   );
   localStorage.setItem("geoJsonMap", "false");
 }
@@ -71,8 +109,8 @@ if (localStorage.getItem("geoJsonMap") === "true") {
 if (localStorage.getItem("theme") === null) {
   console.log(
     `localstorage theme is ${localStorage.getItem(
-      "theme",
-    )}, defaulting to system.`,
+      "theme"
+    )}, defaulting to system.`
   );
   localStorage.setItem("theme", "system");
 }
@@ -105,7 +143,7 @@ if (localStorage.getItem("movableMap") === "true") {
   mapPan = false;
 } else {
   console.log(
-    `movableMap is ${localStorage.getItem("movableMap")}, defaulting to false`,
+    `movableMap is ${localStorage.getItem("movableMap")}, defaulting to false`
   );
   localStorage.setItem("movableMap", "false");
 }
@@ -116,7 +154,7 @@ window
     userTheme = event.matches ? "dark" : "light";
     if (localStorage.getItem("theme") === "system") {
       console.log(
-        "User theme changed and the setting is system, refreshing...",
+        "User theme changed and the setting is system, refreshing..."
       );
       dimScreenAndReload("user changed system theme");
     }
@@ -313,7 +351,7 @@ const getScaleColor = (scale) => {
 const createDeflatedIcon = (scale) => {
   return leaflet.divIcon({
     html: `<div style="background-color: ${getScaleColor(
-      scale,
+      scale
     )}; width: 10px; height: 10px; border-radius: 50%;"></div>`,
     className: "deflated-marker",
     iconSize: [10, 10],
@@ -386,17 +424,36 @@ const updateMapWithData = async (earthquakeData) => {
     if (usegeojson === true) {
       // skipcq: JS-0125 ignore this because it is fetched from a cdn
       omnivore
-        .topojson("https://pickingname.github.io/basemap/subPrefsTopo.json")
+        .geojson(
+          "https://raw.githubusercontent.com/Ameuma773/EarthQuicklyForWeb/main/Source/Cities.json"
+        )
         .on("ready", function () {
           this.eachLayer((layer) => {
+            const center = turf.center(layer.toGeoJSON());
+            const centerLat = center.geometry.coordinates[1];
+            const centerLon = center.geometry.coordinates[0];
+
+            const intensity = calculateJMAIntensity(
+              earthquakeData.earthquake.hypocenter.latitude,
+              earthquakeData.earthquake.hypocenter.longitude,
+              earthquakeData.earthquake.hypocenter.depth,
+              earthquakeData.earthquake.hypocenter.magnitude,
+              centerLat,
+              centerLon
+            );
+
             layer.setStyle({
-              color: userTheme === "dark" ? "#bebebe" : "#969e9e",
+              color: "#000000",
               weight: 1,
               smoothFactor: 0.0,
               fill: true,
-              fillColor: userTheme === "dark" ? "#FFFFFF" : "#95999b",
-              fillOpacity: 0.1,
+              fillColor: intensityColors[intensity],
+              fillOpacity: 0.7,
             });
+
+            layer.bindPopup(
+              `Region: ${layer.feature.properties.regionname}<br>Intensity: ${intensity}`
+            );
           });
         })
         .addTo(mapInstance);
@@ -427,7 +484,7 @@ const updateMapWithData = async (earthquakeData) => {
           `https://{s}.basemaps.cartocdn.com/${userTheme}_all/{z}/{x}/{y}{r}.png`,
           {
             maxZoom: 24,
-          },
+          }
         )
         .addTo(mapInstance);
     } else {
@@ -436,7 +493,7 @@ const updateMapWithData = async (earthquakeData) => {
           `https://{s}.basemaps.cartocdn.com/${userTheme}_all/{z}/{x}/{y}{r}.png`,
           {
             maxZoom: 24,
-          },
+          }
         )
         .addTo(mapInstance);
     }
@@ -526,18 +583,18 @@ const updateMapWithData = async (earthquakeData) => {
           earthquakeData.earthquake.hypocenter.latitude,
           earthquakeData.earthquake.hypocenter.longitude,
         ],
-        { icon: epicenterIcon },
+        { icon: epicenterIcon }
       )
       .addTo(markersLayerGroup);
 
     const comparisonData = await fetchComparisonData(
-      "https://pickingname.github.io/basemap/compare_points.csv",
+      "https://pickingname.github.io/basemap/compare_points.csv"
     );
 
     earthquakeData.points.forEach((point) => {
       const stationCoordinates = findStationCoordinates(
         comparisonData,
-        point.addr,
+        point.addr
       );
       if (stationCoordinates) {
         // tempskip
@@ -546,7 +603,7 @@ const updateMapWithData = async (earthquakeData) => {
           {
             icon: createInflatedIcon(point.scale),
             scale: point.scale,
-          },
+          }
         );
         stationMarkersGroup.addLayer(marker);
       }
@@ -554,19 +611,19 @@ const updateMapWithData = async (earthquakeData) => {
   } else {
     isScalePrompt = true;
     const comparisonData = await fetchComparisonData(
-      "https://pickingname.github.io/basemap/prefs.csv",
+      "https://pickingname.github.io/basemap/prefs.csv"
     );
 
     earthquakeData.points.forEach((point) => {
       console.log(`processing point with address: ${point.addr}`);
       const stationCoordinates = findStationCoordinates(
         comparisonData,
-        point.addr,
+        point.addr
       );
       if (stationCoordinates) {
         console.log(
           `found coordinates for ${point.addr}: `,
-          stationCoordinates,
+          stationCoordinates
         );
         // tempskip
         const marker = leaflet.marker(
@@ -574,7 +631,7 @@ const updateMapWithData = async (earthquakeData) => {
           {
             icon: createInflatedIcon(point.scale),
             scale: point.scale,
-          },
+          }
         );
         stationMarkersGroup.addLayer(marker);
       } else {
@@ -666,7 +723,7 @@ const updateTsunamiLayer = (tsunamiData, geojsonData) => {
       ...geojsonData,
       features: geojsonData.features.filter((feature) => {
         const tsunamiArea = tsunamiData.areas.find(
-          (area) => area.name === feature.properties.name,
+          (area) => area.name === feature.properties.name
         );
         return tsunamiArea?.grade;
       }),
@@ -677,7 +734,7 @@ const updateTsunamiLayer = (tsunamiData, geojsonData) => {
       .geoJSON(filteredGeojsonData, {
         style: (feature) => {
           const tsunamiArea = tsunamiData.areas.find(
-            (area) => area.name === feature.properties.name,
+            (area) => area.name === feature.properties.name
           );
           if (tsunamiArea) {
             return {
@@ -710,7 +767,7 @@ const updateTsunamiLayer = (tsunamiData, geojsonData) => {
       updateCamera(bounds);
     } else {
       console.warn(
-        "Invalid bounds after combining tsunamiGeojsonLayer with other layers",
+        "Invalid bounds after combining tsunamiGeojsonLayer with other layers"
       );
     }
   }
@@ -785,7 +842,7 @@ const fetchAndUpdateData = async () => {
         .replace(/\n|\r/g, "");
       console.log(
         "API call successful with response code:",
-        sanitizedResponseStatus,
+        sanitizedResponseStatus
       );
       isApiCallSuccessful = true;
     }
@@ -874,7 +931,7 @@ setInterval(() => {
         updateCamera(bounds);
       } else {
         console.warn(
-          "[fn () setinterval, maphandler.js] No valid bounds for interval camera update",
+          "[fn () setinterval, maphandler.js] No valid bounds for interval camera update"
         );
       }
     }
@@ -886,7 +943,7 @@ setInterval(() => {
         updateCamera(bounds);
       } else {
         console.warn(
-          "[fn eewcheck, maphandler.js] No valid bounds for interval camera update",
+          "[fn eewcheck, maphandler.js] No valid bounds for interval camera update"
         );
       }
     }
