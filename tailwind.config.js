5065ac00c4aae12ec9010fc575261caf15f5f976
pickingname <trintrinir@icloud.com>
2024-09-16 21:32:58 +0700

    refactor: update Tailwind CSS content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: "class",
-  content: ["**/*.{html,js}"],
+  content: ["./index.html", "./src/*.js"],
   theme: {
     extend: {},
   },
